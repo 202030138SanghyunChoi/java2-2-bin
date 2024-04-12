dcfd5e8085a46b26ab8cc98a9ed581ef494e8987
202030138SanghyunChoi <shshchoi@naver.com>
2024-04-12 16:41:43 +0900

    week6(1)

---
+++ App/src/ColorPointEx.java
@@ -0,0 +1,48 @@
+class Point {
+    private int x, y;
+    public Point() {
+        this.x = this.y = 0;
+    }
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public void set(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public void showPoint() {
+        System.out.println("(" + x + "," + y + ")");
+    }
+}
+
+class ColorPoint extends Point {
+    private String color;
+    public ColorPoint() {
+
+    }
+    public ColorPoint(int x, int y, String color) {
+        super(x, y);
+        this.color = color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void showColorPoint() {
+        System.out.print(color);
+        showPoint();
+    }
+}
+
+public class ColorPointEx {
+    public static void main(String[] args) {
+        Point p = new Point();
+        p.set(1, 2);
+        p.showPoint();
+
+        ColorPoint cp = new ColorPoint();
+        cp.set(3, 4);
+        cp.setColor("red");
+        cp.showColorPoint();
+    }
+}
---
+++ App/src/StaticTest.java
@@ -0,0 +1,24 @@
+class StaticSample {
+    int n;
+    void g() {
+        System.out.println("인스턴스 함수 실행");
+    }
+    static int m;
+    static void f() {
+        System.out.println("스태틱 함수 실행");
+    }
+}
+
+public class StaticTest {
+    public static void main(String[] args) {
+        StaticSample.m = 3;
+        StaticSample.f();
+
+        StaticSample b1 = new StaticSample();
+        // 원칙적으론 사용하지 말 것(클래스로 사용하면 static 멤버인 것을 확실히 알지만 객체로 사용하면 혼동될 수 있음)
+        b1.m = 6;
+        b1.f();
+
+        System.out.println(StaticSample.m);
+    }
+}
