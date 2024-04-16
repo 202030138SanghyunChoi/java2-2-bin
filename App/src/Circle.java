2b05cfb6784ddef08615675db132b0c1d4fbe89a
JBK <149214893+JaeBinary@users.noreply.github.com>
2024-04-16 19:21:00 +0900

    Update Circle.java
    1. Circle 클래스 내에 'public Circle() { }'를 한 이유가 무엇입니까?
    
    2. Circle 클래스 내에 'public Circle(int r, String n)'는 메소드이기에 소문자(circleInform)로 표기하고 리턴 값이 없기에 'void'로 명시해주면 좋을 것 같습니다.
    
    3. '3.14' 같은 경우 리터럴 형식보단 상수라고 명시해주는 것이 좋을 것 같습니다.

--- App/src/Circle.java
+++ App/src/Circle.java
@@ -1,31 +1,32 @@
 public class Circle {
-    public Circle() {
-    }
 
-    public Circle(int r, String n) {
+    final static double PI = 3.14;
+    
+    int radius;
+    String name;
+
+    public void circleInfom(int r, String n) {
         radius = r;
         name = n;
     }
 
     public double getArea() {
-        return 3.14 * radius * radius;
+        return radius * radius * PI;
     }
-
-    int radius;
-    String name;
     
     public static void main(String[] args) {
         Circle pizza = new Circle(10, "자바피자");
         double area = pizza.getArea();
         System.out.println(pizza.name + "의 면적은 " + area + "입니다.");
 
-        Circle donut = new Circle();
-        donut.name = "도넛피자";
-        area = donut.getArea();
-        System.out.println(donut.name + "의 면적은 " + area + "입니다.");
-
         Circle pizza2 = new Circle();
+        pizza2.radius = 20; 
         area = pizza2.getArea();
         System.out.println(pizza2.name + "의 면적은 " + area + "입니다.");
+        
+        Circle donut = new Circle();
+        donut.name = "자바도넛";
+        area = donut.getArea();
+        System.out.println(donut.name + "의 면적은 " + area + "입니다.");
     }
 }
