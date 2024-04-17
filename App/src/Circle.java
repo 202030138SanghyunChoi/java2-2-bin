f49a5a3cb45c010da8ff0fed49226a171a81932f
202030138SanghyunChoi <162421869+202030138SanghyunChoi@users.noreply.github.com>
2024-04-17 11:06:07 +0900

    Revert "Update Circle.java"

--- App/src/Circle.java
+++ App/src/Circle.java
@@ -1,32 +1,31 @@
 public class Circle {
+    public Circle() {
+    }
 
-    final static double PI = 3.14;
-    
-    int radius;
-    String name;
-
-    public void circleInfom(int r, String n) {
+    public Circle(int r, String n) {
         radius = r;
         name = n;
     }
 
     public double getArea() {
-        return radius * radius * PI;
+        return 3.14 * radius * radius;
     }
+
+    int radius;
+    String name;
     
     public static void main(String[] args) {
         Circle pizza = new Circle(10, "자바피자");
         double area = pizza.getArea();
         System.out.println(pizza.name + "의 면적은 " + area + "입니다.");
 
-        Circle pizza2 = new Circle();
-        pizza2.radius = 20; 
-        area = pizza2.getArea();
-        System.out.println(pizza2.name + "의 면적은 " + area + "입니다.");
-        
         Circle donut = new Circle();
-        donut.name = "자바도넛";
+        donut.name = "도넛피자";
         area = donut.getArea();
         System.out.println(donut.name + "의 면적은 " + area + "입니다.");
+
+        Circle pizza2 = new Circle();
+        area = pizza2.getArea();
+        System.out.println(pizza2.name + "의 면적은 " + area + "입니다.");
     }
 }
