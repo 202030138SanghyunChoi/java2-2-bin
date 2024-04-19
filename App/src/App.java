00c9f8e2eaf1dba1891c2e88f0be300b79a2c112
202030138SanghyunChoi <shshchoi@naver.com>
2024-04-19 15:43:24 +0900

    week7

--- App/src/App.java
+++ App/src/App.java
@@ -1,5 +1,12 @@
+import java2.Calculator;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int a = 5;
+        int b = 3;
+        Calculator cal = new Calculator();
+        System.out.println(a + " + " + b + " = " + cal.add(a,b));
     }
 }
